9be64e8c7900715a515f73daa5ce651358808bc3
huynxtb <huynx11.dev@gmail.com>
2024-09-14 16:35:54 +0700

    first commit

---
+++ WebhookDiscord/DiscordNotification.cs
@@ -0,0 +1,37 @@
+﻿using RestSharp;
+
+namespace WebhookDiscord;
+
+public static class DiscordNotification
+{
+    public static async Task SendMessageAsync(
+        string webhookUrl,
+        string userName,
+        string avatarUrl,
+        string title,
+        DiscordColor color,
+        string titleUrl,
+        string content)
+    {
+        var client = new RestClient(webhookUrl);
+        var request = new RestRequest() { Method = Method.Post };
+
+        request.AddJsonBody(
+            new
+            {
+                username = userName,
+                avatar_url = avatarUrl,
+                embeds = new List<object>()
+                {
+                    new
+                    {
+                        title = title.ToUpper(),
+                        color = color,
+                        url = titleUrl,
+                        description = content
+                    }
+                }
+            });
+        var response = await client.ExecuteAsync(request);
+    }
+}
---
+++ WebhookDiscord/Program.cs
@@ -0,0 +1,11 @@
+﻿using WebhookDiscord;
+
+var webhookURL = "https://discord.com/api/webhooks/1051086827349610576/4-3aiRLUFfW4nZwD9hk1aaqx3pGT_2Gls1Fc1LWQkmFZneYHJ3BtHgPAS77WM08g_03g";
+var userName = "Code Mega";
+var avatarURL = "https://i.imgur.com/LIorKnU.jpg";
+var title = "Chào các bạn!";
+var color = DiscordColor.Yellow;
+var titleURL = "https://code-mega.com";
+var content = "Welcome to my chanel!!! \n Have a nice day \n **Code Mega**";
+
+await DiscordNotification.SendMessageAsync(webhookURL, userName, avatarURL, title, color, titleURL, content);
